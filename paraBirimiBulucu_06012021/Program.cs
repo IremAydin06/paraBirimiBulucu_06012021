56283fd74b58c06c8f038c6998edddda1f8bda1b
İREM AYDIN <iaydin0698@hotmail.com>
2021-01-09 18:11:35 +0300

    Add project files.

---
+++ paraBirimiBulucu_06012021/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paraBirimiBulucu_06012021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ülke giriniz (tr, fr, abd, it, hol)");
+            string ulkeKodu = Console.ReadLine();
+            /*i if ile
+             
+            if (ulkeKodu == "tr")
+            {
+                Console.WriteLine("Lira");
+            }
+            else if (ulkeKodu == "fr")
+            {
+                Console.WriteLine("Euro");
+            }
+            else if (ulkeKodu == "abd")
+            {
+                Console.WriteLine("Dolar");
+            }
+            else
+            {
+                Console.WriteLine("Yok ki");
+            }
+            */
+
+            switch (ulkeKodu)
+            {
+                case "tr":
+                    Console.WriteLine("Lira");
+                    break;
+
+                case  "fr":
+                case  "it":
+                case "hol":
+                    Console.WriteLine("Euro");
+                    break;
+
+                case "abd":
+                    Console.WriteLine("Dolar");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
